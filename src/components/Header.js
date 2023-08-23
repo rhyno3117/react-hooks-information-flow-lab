2452ef395452b60cfb3b868aafb4f06f3cf59dff
Ryan Meyers <rhynogolfer42@gmail.com>
2023-08-22 21:28:36 -0600

    Adds myfile.md

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+//Create a Header component by refactoring the <header> element out of the App component. 
+//Clicking on the <button> inside of the Header component should still toggle dark mode on and off.
+//You will need to pass a callback function as a prop called onDarkModeClick to the Header component 
+//in order for the test to pass.
+
+import React from "react";
+
+function Header({isDarkMode, onDarkModeClick}) {
+    return (
+        <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+    )
+}
+
+export default Header;
